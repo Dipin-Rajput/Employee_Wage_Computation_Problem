fd83f464584a96ce1a51a0a8aeaf9ce3df50c364
Dipin-Rajput India <dipinrajput707@gmail.com>
2024-12-20 12:57:40 +0530

    [Dipin]Update: Updated EmlopyeeWageProblem.py file

--- EmployeeWageProblem.py
+++ EmployeeWageProblem.py
@@ -15,40 +15,39 @@ def check_attendance():
         return attendance
 
 welcome_message()
-status = check_attendance()
+# check_attendance()
 
 #UC2
 
 wage_per_hour = 20
 full_day_hours = 8
 
-def calculate_daily_wage():
+def full_day_wages():
+
+    status = check_attendance()
 
     if(status == 1):
         daily_wages = full_day_hours * wage_per_hour
         return daily_wages
     else:
-        print("You are absent today.")
+        return None
 
-daily_wages = calculate_daily_wage()
-print("Your today's wages:", daily_wages)
+fd_wages = full_day_wages()
+print("Your full-day wages are:", fd_wages)
 
 #UC3
 
 part_time_hours = 8
 
-def calculate_total_wage():
+def part_time_wages():
+
+    status = check_attendance()
 
     if(status == 1):
-        print("Enter 1 for part-time performed.")
-        print("Enter 0 for part-time not performed.")
-        part_time = int(input("Enter you input: "))
+        daily_wages = part_time_hours * wage_per_hour
+        return daily_wages
+    else:
+        return None
 
-        if(part_time == 1):
-            amount = daily_wages + part_time_hours * wage_per_hour
-            print("Your today's wages are:", amount)
-        else:
-            wages = calculate_daily_wage()
-            print("Your today's wages are:", wages)
-
-calculate_total_wage()
+pt_wages = part_time_wages()
+print("Your part-time wages are:", pt_wages)
