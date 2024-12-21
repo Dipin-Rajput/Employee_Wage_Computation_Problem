48a4ac862b84339e43e6ab2b1bf5a9f9922aca1e
Dipin-Rajput India <dipinrajput707@gmail.com>
2024-12-21 17:45:38 +0530

    [Dipin]Update: Updated EmployeeWageProblem.py file

--- EmployeeWageProblem.py
+++ EmployeeWageProblem.py
@@ -91,6 +91,51 @@ def monthly_wages():
     
     return total_wages
 
+#UC6
+
+max_working_hours = 100
+max_working_days = 20
+
+def wages_with_conditions():
+
+    total_days = 1
+    total_hours = 0
+    total_wages = 0
+
+    while(total_hours < max_working_hours and total_days < max_working_days + 1):
+
+        fd_status = check_attendance()
+
+        if(fd_status == 1):
+
+            hours_worked = full_day_hours
+            total_hours += hours_worked
+            total_wages += hours_worked * wage_per_hour
+
+            print(f"Your Day {total_days} full-day wages are:", hours_worked * wage_per_hour)
+        
+        else:
+            print(f"Your Day {total_days} full-day wages are:", None)
+        
+        pt_status = check_attendance()
+
+        if(pt_status == 1):
+
+            hours_worked = random.randrange(1, 9)
+            total_hours += hours_worked
+            total_wages += hours_worked * wage_per_hour
+
+            print(f"Your Day {total_days} part-time wages are:", hours_worked * wage_per_hour)
+
+        else:
+            print(f"Your Day {total_days} part-time wages are:", None)
+        
+        print()
+
+        total_days += 1
+    
+    return total_wages
+
 #UC4
 
 print()
@@ -98,6 +143,7 @@ print("Enter 1 to check attendance status.")
 print("Enter 2 to check full-day wages.")
 print("Enter 3 to check part-time wages.")
 print("Enter 4 to check monthly wages.")
+print("Enter 5 to check wages based on condition of 100 hours and 20 days.")
 print()
 
 choice = int(input("Enter your choice: "))
@@ -112,5 +158,8 @@ match choice:
         print("Your part-time wages are:", part_time_wages())
     case 4:
         print("Your monthly wages are:", monthly_wages())
+    case 5:
+        print("Your total wages based on condition are:", wages_with_conditions())
     case _:
         print("Please select from above choices only.")
+
